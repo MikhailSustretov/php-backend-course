2e044b50e35de8124eed5938f1bdc75e4b8002c8
MikhailSustretov <95348185+MikhailSustretov@users.noreply.github.com>
2022-02-17 13:57:13 +0200

    Delete application directory

--- application/etc/cron/db_back_up_cron/back_up.php
+++
@@ -1,4 +0,0 @@
-<?php
-$db_params = require_once '../../../../application/config/db_params.php';
-
-exec("D:\WNMP\mysql\bin\mysqldump -u{$db_params['db_user']} -p{$db_params['db_pass']} {$db_params['db_name']} > backups/" . date('d.m.Y') . ".sql");
--- application/etc/cron/db_back_up_cron/back_up_cron.bat
+++
@@ -1,2 +0,0 @@
-D:\WNMP\php\php.exe D:\WNMP\nginx\html\libraryApp\application\etc\cron\db_back_up_cron\back_up.php
-pause
